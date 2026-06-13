6584f30ae6543ad3e727326701f32cce31b2edaa
Nurudeen <nurudeenodofin85@gmail.com>
2026-06-13 15:24:50 +0100

    changes to follow

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const frame = document.getElementById("img_fram");
-const story = document.getElementById("storyShow")
+const story = document.getElementById("storyShow");
+const closeStory = document.getElementById("closeStory");
+const followers = document.getElementById("follows");
+const showFollowers = document.getElementById("showFollower");
+const closeView = document.getElementById("closeView");
 
  function showStory() {
     frame.addEventListener("click",()=> {
- story.classList.toggle("showi");   
+      story.style.display = "block";
          });
+
+     closeStory.addEventListener("click",()=> {
+        story.style.display = "none";
+     });
+
+     followers.addEventListener("click",()=> {
+        showFollowers.style.display = "block";
+     });
+
+     closeView.addEventListener("click",()=> {
+       showFollowers.style.display = "none";
+     });
+ 
  }
 
  showStory();
+
+ 
