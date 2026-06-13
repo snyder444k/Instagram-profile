dc92e33c74e8f92c56dc43180ce5b30d0b2b4419
Nurudeen <nurudeenodofin85@gmail.com>
2026-06-13 16:24:48 +0100

    changes to following

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const closeStory = document.getElementById("closeStory");
 const followers = document.getElementById("follows");
 const showFollowers = document.getElementById("showFollower");
 const closeView = document.getElementById("closeView");
+const following = document.getElementById("followi");
+const showFollowi = document.getElementById("showFollowi");
+const closeViews = document.getElementById("closeViews");
+
 
  function showStory() {
     frame.addEventListener("click",()=> {
@@ -20,6 +24,14 @@ const closeView = document.getElementById("closeView");
 
      closeView.addEventListener("click",()=> {
        showFollowers.style.display = "none";
+     });
+
+     following.addEventListener("click",()=> {
+      showFollowi.style.display = "block";
+   }); 
+
+      closeViews.addEventListener("click",()=> {
+       showFollowi.style.display = "none";
      });
  
  }
